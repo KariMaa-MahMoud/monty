fbeea155a7519d0b4d956b697a738c8e41b16abf
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2023-12-15 01:56:05 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -38,3 +38,45 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/*file operations*/
+void read_file(FILE *);
+void open_file(char *file_name);
+void find_func(char *, char *, int, int);
+int parse_line(char *buffer, int line_number, int format);
+int len_chars(FILE *);
+
+/*Errors*/
+void error(int error_code, ...);
+void more_error(int error_code, ...);
+void string_error(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
+/*Strings*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+void call_fun(op_func, char *, char *, int, int);
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+/*Math operations*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+#endif
